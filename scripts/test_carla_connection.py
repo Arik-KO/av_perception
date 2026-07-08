d9e7dffb6bf9e0eedf390155d751fc4a1d374b78
Arik-KO <arikanik40@gmail.com>
2026-07-08 12:28:17 +0800

    fixed: carla connection testing generalized

--- scripts/test_carla_connection.py
+++ scripts/test_carla_connection.py
@@ -6,5 +6,5 @@ from av_perception.utils.carla_client import connect
 logger = setup_logger()
 
 
-client = connect(host= "172.23.64.1")
+client = connect(host= "write_host_address")
 print("CARLA connection is working!")
